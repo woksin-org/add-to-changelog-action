05009c33c964c85469372ac388f04976017853dc
Joel Hoisko <joel.hoisko@gmail.com>
2021-01-25 15:24:34 +0100

    Add changelog and action to prepend to it

--- Source/action.ts
+++ Source/action.ts
@@ -1,22 +1,52 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import * as core from '@actions/core';
+import { getInput, setFailed } from '@actions/core';
 import { Logger } from '@dolittle/github-actions.shared.logging';
+import { readFile, writeFile } from 'fs';
 
 const logger = new Logger();
 
 run();
 export async function run() {
     try {
-        // Put your code in here
+        const version = getInput('version', { required: true });
+        const body = getInput('body', { required: true });
+        const prUrl = getInput('pr-url', { required: true });
+        const changelogPath = getInput('changelog-path', { required: true });
+
+        logger.info(`Creating new content for changelog with version ${version}`);
+        const content = createChangelogContent(body, version, prUrl);
+
+        logger.info(`Writing to path ${changelogPath} with heading ${content[0]} and ${content.length} lines of new content`);
+        writeToFile(changelogPath, content);
+
+        logger.info('Write complete');
     } catch (error) {
         fail(error);
     }
 }
 
-
 function fail(error: Error) {
     logger.error(error.message);
-    core.setFailed(error.message);
+    setFailed(error.message);
+}
+
+function createChangelogContent(body: string, version: string, prUrl: string): string[] {
+    const date = new Date(new Date().toUTCString());
+    const heading = `# [${version}] - ${date.getFullYear}-${date.getMonth}-${date.getDate} (PR: ${prUrl})`;
+    const splitBody = body.split('\n');
+    return [heading, ...splitBody];
+}
+
+function writeToFile(filePath: string, content: string[]) {
+    readFile(filePath, (err, data) => {
+        if (err) fail(err);
+        const oldContent = data.toString().split('\n');
+        oldContent.unshift(...content);
+        const newContent = oldContent.join('\n');
+        writeFile(filePath, newContent, (err) => {
+            if (err) fail(new Error(err.message));
+        });
+    });
 }
